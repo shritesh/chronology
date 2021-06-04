ec905c64404e9947f7bafe47754d35ac8364f354
Shritesh Bhattarai <shr@ite.sh>
2021-06-04 17:11:23 -0400

    Add Iced UI

--- src/lib.rs
+++ src/lib.rs
@@ -8,31 +8,31 @@ pub struct Category {
 }
 
 impl Category {
-    pub async fn new(executor: &SqlitePool, title: &str) -> Result<Self> {
+    pub async fn new(pool: SqlitePool, title: &str) -> Result<Self> {
         sqlx::query_as!(
             Self,
             r#"insert into category(title) values (?) returning id as "id!: i64", title as "title!: String""#,
             title
         )
-        .fetch_one(executor)
+        .fetch_one(&pool)
         .await
     }
 
-    pub async fn all(executor: &SqlitePool) -> Result<Vec<Self>> {
+    pub async fn all(pool: SqlitePool) -> Result<Vec<Self>> {
         sqlx::query_as!(Self, r#"select * from category"#)
-            .fetch_all(executor)
+            .fetch_all(&pool)
             .await
     }
 
-    pub async fn get(executor: &SqlitePool, id: i64) -> Result<Self> {
+    pub async fn get(pool: SqlitePool, id: i64) -> Result<Self> {
         sqlx::query_as!(Self, r#"select * from category where id = ?"#, id)
-            .fetch_one(executor)
+            .fetch_one(&pool)
             .await
     }
 
-    pub async fn tasks(&self, executor: &SqlitePool) -> Result<Vec<Task>> {
+    pub async fn tasks(&self, pool: SqlitePool) -> Result<Vec<Task>> {
         sqlx::query_as!(Task, r#"select * from task where category_id = ?"#, self.id)
-            .fetch_all(executor)
+            .fetch_all(&pool)
             .await
     }
 }
@@ -45,26 +45,26 @@ pub struct Task {
 }
 
 impl Task {
-    pub async fn new(executor: &SqlitePool, title: &str, category: &Category) -> Result<Self> {
+    pub async fn new(pool: SqlitePool, title: &str, category: &Category) -> Result<Self> {
         sqlx::query_as!(
             Self,
             r#"insert into task(title, category_id) values (?, ?) returning id as "id!: i64", title as "title!: String", category_id as "category_id!: i64""#,
             title,
             category.id
         )
-        .fetch_one(executor)
+        .fetch_one(&pool)
         .await
     }
 
-    pub async fn all(executor: &SqlitePool) -> Result<Vec<Self>> {
+    pub async fn all(pool: SqlitePool) -> Result<Vec<Self>> {
         sqlx::query_as!(Self, r#"select * from task"#)
-            .fetch_all(executor)
+            .fetch_all(&pool)
             .await
     }
 
-    pub async fn get(executor: &SqlitePool, id: i64) -> Result<Self> {
+    pub async fn get(pool: SqlitePool, id: i64) -> Result<Self> {
         sqlx::query_as!(Self, r#"select * from task where id = ?"#, id)
-            .fetch_one(executor)
+            .fetch_one(&pool)
             .await
     }
 }
@@ -79,7 +79,7 @@ pub struct Entry {
 }
 
 impl Entry {
-    pub async fn new_now(executor: &SqlitePool, task: &Task) -> Result<Self> {
+    pub async fn new_now(pool: SqlitePool, task: &Task) -> Result<Self> {
         let now = Utc::now();
         sqlx::query_as!(
             Self,
@@ -87,13 +87,13 @@ impl Entry {
             task.id,
             now
         )
-        .fetch_one(executor)
+        .fetch_one(&pool)
         .await
     }
 
-    pub async fn all(executor: &SqlitePool) -> Result<Vec<Self>> {
+    pub async fn all(pool: SqlitePool) -> Result<Vec<Self>> {
         sqlx::query_as!(Self, r#"select id, task_id, start_time as "start_time!: DateTime<Utc>", end_time as "end_time?: DateTime<Utc>", note from entry"#)
-            .fetch_all(executor)
+            .fetch_all(&pool)
             .await
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,98 @@
-use chronology::Entry;
-use sqlx::{Result, SqlitePool};
+use chronology::Category;
+use iced::{Application, Command, Settings, Text};
+use sqlx::{Error, Result, SqlitePool};
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let pool = SqlitePool::connect("sqlite:chronology.db").await?;
+struct State {
+    pool: SqlitePool,
+    categories: Option<Vec<Category>>,
+}
 
-    for entry in Entry::all(&pool).await? {
-        dbg!(entry);
+enum Ui {
+    Loading,
+    Loaded(State),
+    Error(Error),
+}
+
+#[derive(Debug)]
+enum Msg {
+    Init(Result<SqlitePool>),
+
+    FetchCategoryAll(Result<Vec<Category>>),
+}
+
+impl Application for Ui {
+    type Executor = iced::executor::Default;
+
+    type Message = Msg;
+
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        (
+            Ui::Loading,
+            Command::perform(SqlitePool::connect("sqlite:chronology.db"), Msg::Init),
+        )
     }
 
-    Ok(())
+    fn title(&self) -> String {
+        "Chronology".to_string()
+    }
+
+    fn update(
+        &mut self,
+        message: Self::Message,
+        _clipboard: &mut iced::Clipboard,
+    ) -> Command<Self::Message> {
+        match self {
+            Ui::Loading => match message {
+                Msg::Init(Ok(pool)) => {
+                    *self = Ui::Loaded(State {
+                        pool: pool.clone(),
+                        categories: None,
+                    });
+                    Command::perform(Category::all(pool), Msg::FetchCategoryAll)
+                }
+                Msg::Init(Err(err)) => {
+                    *self = Ui::Error(err);
+
+                    Command::none()
+                }
+                _ => Command::none(),
+            },
+
+            Ui::Loaded(state) => match message {
+                Msg::Init(_) => panic!("Impossible"),
+                Msg::FetchCategoryAll(Ok(categories)) => {
+                    state.categories = Some(categories);
+                    Command::none()
+                }
+                Msg::FetchCategoryAll(Err(err)) => {
+                    *self = Ui::Error(err);
+                    Command::none()
+                }
+            },
+
+            Ui::Error(_) => Command::none(),
+        }
+    }
+
+    fn view(&mut self) -> iced::Element<'_, Self::Message> {
+        let label = match &self {
+            Ui::Loading => "Loading".to_string(),
+            Ui::Loaded(state) => {
+                if let Some(ref categories) = state.categories {
+                    format!("{} categories", categories.len())
+                } else {
+                    format!("No categories found")
+                }
+            }
+            Ui::Error(err) => err.to_string(),
+        };
+
+        Text::new(label).into()
+    }
+}
+
+fn main() -> iced::Result {
+    Ui::run(Settings::default())
 }
